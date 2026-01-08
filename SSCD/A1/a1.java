5500a29aaf75d05689df70be2c41291f7ec1f41c
Kalpak26 <kalpak26.3.04@gmail.com>
2026-01-08 14:54:13 +0530

    Update a1.java

--- SSCD/A1/a1.java
+++ SSCD/A1/a1.java
@@ -1,12 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+
+class Opcode {
+    String type;   // IS, AD, DL
+    int code;
+
+    Opcode(String type, int code) {
+        this.type = type;
+        this.code = code;
+    }
+}
 
 public class a1 {
 
+    static HashMap<String, Opcode> OPTAB = new HashMap<>();
+
     public static void main(String[] args) {
 
-        String fileName = "input.asm";   // Assembly source file
+        // Read & Display Source File
+        String fileName = "input.asm";
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -22,6 +36,28 @@ public class a1 {
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
+
+        // Load OPTAB
+        loadOPTAB();
+        // Display OPTAB
+        System.out.println("----- OPTAB -----");
+        for (String key : OPTAB.keySet()) {
+            Opcode op = OPTAB.get(key);
+            System.out.println(key + " -> (" + op.type + ", " + op.code + ")");
+        }
+    }
+
+    static void loadOPTAB() {
+        OPTAB.put("ADD",   new Opcode("IS", 1));
+        OPTAB.put("SUB",   new Opcode("IS", 2));
+        OPTAB.put("MOVER", new Opcode("IS", 3));
+        OPTAB.put("MOVEM", new Opcode("IS", 4));
+        OPTAB.put("PRINT", new Opcode("IS", 5));
+        OPTAB.put("READ",  new Opcode("IS", 6));
+        OPTAB.put("START",  new Opcode("AD", 1));
+        OPTAB.put("END",    new Opcode("AD", 2));
+        OPTAB.put("DC",   new Opcode("DL", 1));
+        OPTAB.put("DS",  new Opcode("DL", 02));
     }
 }
 
